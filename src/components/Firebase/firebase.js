3e153015d2ec1ae914a7fd6de3d403b956ac3b3a
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-06-24 19:53:11 +0200

    Configuration Firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAidu_MdRig0CKtQRkQVW4XXeaZRJ8RY-0",
+  authDomain: "les-argonautes-28a9d.firebaseapp.com",
+  projectId: "les-argonautes-28a9d",
+  storageBucket: "les-argonautes-28a9d.appspot.com",
+  messagingSenderId: "718872902378",
+  appId: "1:718872902378:web:2696c4453114fb3cd77593"
+};
+
+const app = initializeApp(firebaseConfig);
